7aae1093d1969a4f49de68f037c3b9c1b30207f2
jyoung105 <jeongmin1604@gmail.com>
2026-06-01 07:26:12 +0900

    feat(native-bridge): Install native bridge at startup

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installNativeBridge } from "./nativeBridge";
 import "./styles.css";
+
+installNativeBridge();
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
